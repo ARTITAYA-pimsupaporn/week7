e185822c9615b8f8f5d2519d7c62b459214c905a
DELL <DELL@DESKTOP-7K6E1DD>
2021-09-19 08:42:58 +0700

    Add project files.

---
+++ week7/66.2.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//จงเขียนโปรแกรมเพื่อรับข้อมูลเป็น string ไปเรื่อยๆ จนกว่าจะพิมพ์เครื่องหมาย . หลังจากนั้นให้จัดเก็บข้อมูลลงในไฟล์ชื่อ c:\temp\data.txt ดังตัวอย่าง (Level 4)
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    FILE* a;
+    char b[10000];
+    int x = 0;
+    a = fopen("C:\\temp\\data.txt", "w+");
+    printf("Input data string:\n");
+    while (1)
+    {
+        scanf("%s", b);
+        x = strlen(b) - 1;
+        fprintf(a, b);
+        fprintf(a, "\n");
+        if (b[x] == '.')
+        {
+            break;
+        }
+    }
+    fclose(a);
+    return 0;
+}
---
+++ week7/66.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//จงเขียนโปรแกรมเพื่อรับข้อมูลเป็น string ไปเรื่อยๆ จนกว่าจะพิมพ์เครื่องหมาย . หลังจากนั้นให้จัดเก็บข้อมูลลงในไฟล์ชื่อ c:\temp\data.txt ดังตัวอย่าง (Level 4)
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    FILE* a;
+    char b[10000];
+    int x = 0,i = 0;
+    a = fopen("C:\\temp\\data.txt", "w+");
+    printf("Input data string:\n");
+    while (1)
+    {
+        scanf("%s", b);
+        for (i = 0; i < strlen(b); i++) {
+            putc(b[i], a);
+        }
+        fprintf(a, "\n");
+        x = strlen(b) - 1;
+        if (b[x] == '.')
+        {
+            break;
+        }
+    }
+    fclose(a);
+    return 0;
+}
